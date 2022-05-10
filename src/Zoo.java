35fdb128ce27456e1279aec5df28619c4d5491a7
Vladyslav <vlad.kirovograd@gmail.com>
2022-05-10 15:15:01 +0300

    HW 6

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,16 +1,19 @@
 import java.util.List;
 
 public class Zoo {
-
   /**
    * Feeds different bird sections of the zoo.
    *
    * @param list list of birds of particular type.
    */
-  public static void feed(List list) {
+  public static void feed(List<? extends Bird> list) {
     // TODO fix method declaration
     System.out.println("Feeding birds");
     // TODO feed birds in the cycle
+    for (Bird bird : list) {
+      System.out.print(bird + " - ");
+      bird.eat();
+    }
   }
 
   /**
@@ -19,11 +22,17 @@ public class Zoo {
    * @param list list of birds of particular type.
    * @param bird a new bird.
    */
-  public static void acceptBird(List list, FlyingBird bird) {
+  public static void acceptBird(List<? super FlyingBird> list, FlyingBird bird) {
     // TODO fix method declaration
-    System.out.println("Accepting a bird to a section");
+    System.out.println("Accepting a bird to a section - " + bird);
     // TODO check flying bird wings
+    bird.checkWings();
     // TODO add a bird to the list and print it
+    list.add(bird);
+    System.out.println("Birds in the list:");
+    list.forEach(System.out::println);
+
+
   }
 
   /**
@@ -32,10 +41,14 @@ public class Zoo {
    * @param list    list of birds.
    * @param newBird bird to add.
    */
-  public static void registerBird(List list, Bird newBird) {
+  public static void registerBird(List<? super Bird> list, Bird newBird) {
     // TODO fix method declaration
     System.out.println("Adding a bird to the birds list");
     // TODO add a bird to list
+    list.add(newBird);
     // TODO print birds in the cycle
+    list.forEach(System.out::println);
+
   }
+
 }
